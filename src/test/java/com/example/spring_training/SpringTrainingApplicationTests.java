3c42bc6bc1945e4860b313865133b111661ff120
SSteinmayer <samira.steinmayer@adesso.de>
2022-07-07 17:44:20 +0200

    initializing SpringBoot Project with Gradle - first commit

---
+++ src/test/java/com/example/spring_training/SpringTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
